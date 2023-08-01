2754e09cf1f50afddd7eafd01fa01366e576cb2d
Lee Hak Jun <harleybird23@gmail.com>
2023-08-01 22:20:55 +0900

    Init Lua

---
+++ LuaTest/LuaTest.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+
+
+extern "C"
+{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+
+#pragma comment (lib, "lua5.1.lib")
+
+using namespace std;
+
+int main()
+{
+    lua_State* L = lua_open();
+    luaL_openlibs(L);
+
+    int res = luaL_dofile(L, "../Script/func.lua");
+
+    lua_getglobal(L, "LuaAdd");
+    lua_pushnumber(L, 30);
+    lua_pushnumber(L, 100);
+    lua_call(L, 2, 1);
+
+    int num = lua_tointeger(L, lua_gettop(L));
+
+    cout << num << endl;
+
+    lua_close(L);
+}
---
+++ Script/func.lua
@@ -0,0 +1,5 @@
+-- func.lua
+
+function LuaAdd( num1, num2 )
+    return num1 + num2
+end
